08bd03130b7bf8b6a968d8d9db16a80b15e8f828
Muhammad Hamza <hamzanair87@gmail.com>
2024-04-12 22:58:39 +0500

    Number Guessing

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessnumber",
+        type: "number",
+        message: "please guess a number between 1-6",
+    },
+]);
+if (answers.userguessnumber === randomnumber) {
+    console.log("congratulation! Your Guessed Number Is Right");
+}
+else {
+    console.log("Oh No! You Guessed A Wrong Number");
+}
